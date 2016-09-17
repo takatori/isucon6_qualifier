c918cbffac60a0acb76dcf4328102e0b089d8824
takatori <takatorisatoshi@team-lab.com>
2016-09-17 16:28:13 +0900

    error処理追加

--- isuda.go
+++ isuda.go
@@ -395,8 +395,9 @@ func getSession(w http.ResponseWriter, r *http.Request) *sessions.Session {
 func starsPostHandler(w http.ResponseWriter, r *http.Request) {
 	keyword := r.URL.Query().Get("keyword")
 	user := r.URL.Query().Get("user")
-	rows := db.Exec(`select keyword from entry where keyword = ?`, keyword)
-	
+	rows, err := db.Exec(`select keyword from entry where keyword = ?`, keyword)
+
+	panicIf(err)
 	if rows.Next() == nil {
 		notFound(w)
 		return
